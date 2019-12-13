01e44a13789b995f7c206dec23a0a0f0c0cdb5ba
Tran Nguyen <16131104@student.hcmute.edu.vn>
2019-12-13 09:43:09 +0700

    fetch admin>manage>product

---
+++ Front/src/app/containers/services/products/products.ts
@@ -0,0 +1,19 @@
+export interface Products {
+    id: number,
+    createdBy: string,
+    createdDate: Date,
+    modifiedBy: string,
+    modifiedDate: Date,
+    name: string,
+    brand: string,
+    price: number,
+    promoPrice: number,
+    series: string,
+    warranty: string,
+    color: string,
+    description:string,
+    rating: string,
+    thumbnail: string,
+    categoryCode: string,
+    category_id: number
+}
--- ShopLinhKien/src/main/java/com/example/shoplinhkien/controllers/UserController.java
+++ ShopLinhKien/src/main/java/com/example/shoplinhkien/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.example.shoplinhkien.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.shoplinhkien.dto.UsersDTO;
 import com.example.shoplinhkien.service.IUserService;
-
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", exposedHeaders = "Authorization")
 @RestController
 public class UserController {
 	
